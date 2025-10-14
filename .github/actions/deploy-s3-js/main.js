c7367d03e101235250d0664f85778a64632f18e0
ngostal2019 <ngomastaline@gmail.com>
2025-10-14 17:35:37 -0500

    Fixed typo with required

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,5 +1,5 @@
 const core =  require('@actions/core');
-const github = required('@actions/github');
+const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
