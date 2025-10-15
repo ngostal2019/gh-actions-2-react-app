2f63824a6739216e54d10d31961f259552a422f9
Staline <51853186+ngostal2019@users.noreply.github.com>
2025-10-15 18:57:37 -0500

    Added logic to visit website from Actions (#11)

---
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -0,0 +1,23 @@
+import os
+import boto3
+from botocore.config import Config as cfg
+
+def run():
+    bucket_name = os.environ['INPUT_BUCKET-NAME']
+    bucket_region = os.environ['INPUT_BUCKET-REGION']
+    dist_older = os.environ['INPUT_DIST-FOLDER']
+
+    configuration = cfg(region_name=bucket_region)
+
+    s3_client = boto3.client('s3', config=configuration)
+
+    for root, subdirs, files in os.walk(dist_older):
+        for file in files:
+            s3_client.upload_file(os.path.join(root, file), bucket_name, file)
+
+    website_url = f'http://{bucket_name}.s3-website-{bucket_region}.amazonaws.com'
+    # print(f'::set-output name=website-url::{website_url}')
+    os.system(f'echo \"website-url={website_url}\" >> $GITHUB_OUTPUT')
+
+if __name__ == '__main__':
+    run()
--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -11,7 +11,10 @@ function run() {
     // 2) Upload files to S3
     const s3Uri = `s3://${bucketName}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-    core.notice('Hello from custom JS actions.');
+
+    // 3) Form AWS static Website URL
+    const websiteUrl = `http://${bucketName}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
