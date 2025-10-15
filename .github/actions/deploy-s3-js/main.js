1e7821da79fd2a75521ce0287b2d8f74c21277b7
Staline <51853186+ngostal2019@users.noreply.github.com>
2025-10-14 23:07:38 -0500

    Using custom actions with JS (#10)

---
+++ .github/actions/deploy-s3-js/main.js
@@ -0,0 +1,17 @@
+const core =  require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function run() {
+    // 1) Get some input values
+    const bucketName = core.getInput('bucket-name', { require: true });
+    const bucketRegion = core.getInput('bucket-region', { require: true });
+    const distFolder = core.getInput('dist-folder', { require: true });
+
+    // 2) Upload files to S3
+    const s3Uri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    core.notice('Hello from custom JS actions.');
+}
+
+run();
